fff23dc99300e35014189d35f2875823f0def56d
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2023-01-13 11:43:24 +0000

    commit tree ee6f78fd1ff6de54b11e83106e1b967028004499

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,30 @@
 #write your code here
+require 'pry'
 
-def countdown
+def countdown int 
+    while int > 0 do
+        puts "#{int} SECOND(S)!"
+        int -= 1
+    end
+    'HAPPY NEW YEAR!'
 end
+
+def countdown_with_sleep int
+    while int > 0 do 
+        puts "#{int} SECOND(S)!"
+        int -= 1
+        sleep(1) 
+    end
+    'HAPPY NEW YEAR!'
+end
+
+countdown 10
+
+
+# takes in an integer argument
+
+# uses while loop to countdown from that integer to 1
+
+# each loop outpus "#{number} SECOND(S)"
+
+# Returns "HAPPY NEW YEAR" after the loop finishes
